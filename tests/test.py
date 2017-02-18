6c5054af72dd10ba448838000969cf77e53d7747
Evgeny Cherkashin <eugene@irnok.net>
2017-02-19 00:48:50 +0800

    Refined recognizer.

--- tests/test.py
+++ tests/test.py
@@ -14,11 +14,24 @@ class icc_fosgenTests:
 
 class TestsSimples:
 
-    def test_extractor(self):
+    #    def test_str_eq(self):
+    #        assert 'ОПК–2' == "ОПК-2"
+
+    def test_extractor_competentions(self):
         from icc.fosgen.constants import extract_comps, COMPETENTIONS
         d = extract_comps(COMPETENTIONS)
         print(d)
         assert len(d) >= 3
+        assert d["ОК-7"]
+        assert d["ОПК-2"]
+        assert d["ОПК-5"]
+
+    def test_extractor_labs(self):
+        from icc.fosgen.constants import extract_work_names, LABS
+        d = extract_work_names(LABS)
+        print(d)
+        assert len(d) >= 5
+        assert d[1]
 
 
 class ExampleOfAIWorkProgram:
